ccd47f98292bbb3f938946d9662c6af30ec02f28
Jeffrey K. Bennett <jeff.bennett@pittsburghmoves.com>
2012-03-16 21:41:14 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '2560820985978e4f782868b128e6ba1b0987d1e995125b7c1909f52d16c82892252eb29265b701a768f3f4f2f813d5eee8fbf2c9a0eaa44e3c7feb65792f7617'
